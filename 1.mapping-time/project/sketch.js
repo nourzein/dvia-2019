a0df7df80c88b727e7db05710438e9a0ffe9211c
Nour Zein <zeinn936@newschool.edu>
2019-09-09 18:51:11 -0400

    first commit

--- 1.mapping-time/project/sketch.js
+++ 1.mapping-time/project/sketch.js
@@ -1,29 +1,41 @@
 
 function setup() {
   // set the width & height of the sketch
-  createCanvas(400, 130)
+  createCanvas(600, 600);
+  angleMode(DEGREES);
+
 
   // print the time to the console once at the beginning of the run. try opening up the
   // web inspector and poking around to see the various values the clock function gives you
-  print('starting time:', clock())
+  // print('starting time:', clock())
 
 }
 
 function draw() {
-  // check the clock for the current time and unpack some of its fields to generate a time-string
-  var now = clock()
+  
+  background(0);
+  translate (300, 300);
+  rotate(-90);
+  let hr= hour();
+  let min= minute();
+  let snd= second();
 
-  // set the background to 'white' – you can also specify colors use integers, hex-color strings and more.
-  // note that setting the background also clears the canvas from our previous round of drawing
-  background('white')
+  let secondsArc = map(snd, 0, 60, 0, 360);
+  let minutesArc= map(min, 0, 60, 0, 360);
+  let hoursArc= map(hr % 12, 0, 12, 0, 360);
+  
 
-  // set up typography & drawing-color
-  textFont("Anonymous Pro") // ← check index.html to see how it was loaded from google-fonts
-  textSize(42) // make it big
-  fill(100, 50, 50)
-
-  // draw the time string to the canvas
-  text(now.text.date, 30, 50)
-  text(now.text.time, 30, 100)
+  background(0)
+  noFill();
+  strokeWeight(8);
+  
+  strokeCap(SQUARE);
+  stroke(225, 52, 235);
+  arc(0, 0, 300, 300, 0, secondsArc); //seconds line
+  arc(0, 0, 335, 335, 0, minutesArc); //minutes line
+  arc (0, 0, 375, 375, 0, hoursArc); //hours line
 
 }
+
+
+  
