7a7a2f280b9be4e8734d512b6a047e64d7c24963
Nour Zein <zeinn936@newschool.edu>
2019-09-16 15:58:58 -0400

    background color

--- 1.mapping-time/clock1/sketch.js
+++ 1.mapping-time/clock1/sketch.js
@@ -22,11 +22,14 @@ function setup() {
 	var minutesArc= map(now.min, 0, 60, 0, 360);
 	var hoursArc= map(now.hour % 12, 0, 12, 0, 360);
 	
-  
 	background(0)
+	fill(66, 21, 55, 95)
+	noStroke();
+	ellipse (0, 0, 400, 400)
+	
+	
 	noFill();
 	strokeWeight(8);
-	
 	strokeCap(SQUARE);
 	stroke(225, 52, 235);
 	arc(0, 0, 300, 300, 0, secondsArc); //seconds line
