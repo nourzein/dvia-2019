a5bb4cd2aab4e7d3f6fe77d47bf6e4f7d1e22e67
Nour Zein <zeinn936@newschool.edu>
2019-10-02 12:54:29 -0400

    clock7

--- 1.mapping-time/clock7_PlanetaryCalendar/sketch.js
+++ 1.mapping-time/clock7_PlanetaryCalendar/sketch.js
@@ -21,36 +21,36 @@ function draw() {
 
   var r = 30;
 
-  // //seasons array
-  // var colors = [
-  //   "#b3330c" /*fall*/,
-  //   "#820717" /*fall*/,
-  //   "#a60707" /*fall*/,
-  //   "#353161" /*winter*/,
-  //   "#311a59" /*winter*/,
-  //   "#691b75" /*winter*/,
-  //   "#f24979" /*spring*/,
-  //   "#fa37f7" /*spring*/,
-  //   "#fa3771" /*spring*/,
-  //   "#2b6e2b" /*summer*/,
-  //   "#d9d445" /*summer*/,
-  //   "#9ed945" /*summer*/
-  // ];
-  // var gradient = chroma.scale(colors).mode("lab");
-  // function colorForProgress(pct) {
-  //   return gradient(pct).hex();
-  // }
+  //seasons array
+  var colors = [
+    "#b3330c" /*fall*/,
+    "#820717" /*fall*/,
+    "#a60707" /*fall*/,
+    "#353161" /*winter*/,
+    "#311a59" /*winter*/,
+    "#691b75" /*winter*/,
+    "#f24979" /*spring*/,
+    "#fa37f7" /*spring*/,
+    "#fa3771" /*spring*/,
+    "#2b6e2b" /*summer*/,
+    "#d9d445" /*summer*/,
+    "#9ed945" /*summer*/
+  ];
+  var gradient = chroma.scale(colors).mode("lab");
+  function colorForProgress(pct) {
+    return gradient(pct).hex();
+  }
 
-  // var color = colorForProgress(now.progress.year);
+  var color = colorForProgress(now.progress.year);
 
-  // r = (now.year - 2018) * r;
-  // console.log(now.year);
+  r = (now.year - 2018) * r;
+  console.log(now.year);
 
   //earth ellipse
   translate(width / 2, height / 2);
   noStroke();
   // fill(5, 99, 35);
-  fill(50, 168, 82);
+  fill(color);
   ellipse(0, 0, 20, 20);
 
   //sun arc
