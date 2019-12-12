12f97d5b2690ee135d2f06f6a0fa6d52cfa2f856
Nour Zein <zeinn936@newschool.edu>
2019-12-12 18:35:14 -0500

    final clock

--- 1.mapping-time/clock7_PlanetaryCalendar/sketch.js
+++ 1.mapping-time/clock7_PlanetaryCalendar/sketch.js
@@ -1,7 +1,7 @@
 //Planets clock
 
 function setup() {
-  createCanvas(800, 800);
+  createCanvas(1000, 1000);
   angleMode(DEGREES);
 }
 
@@ -12,8 +12,11 @@ function draw() {
   var now = clock();
 
   // let sec= now.sec // to check if it's working
-  let day = now.day;
-  let month = now.month;
+  var now = clock();
+
+  var secondsArc = map(now.sec, 0, 60, 0, 360);
+  var minutesArc = map(now.min, 0, 60, 0, 360);
+  var hoursArc = map(now.hour % 12, 0, 12, 0, 360);
   var monthsArc = map(now.month, 0, 12, 0, 360);
   var dayArc = map(now.day, 0, 30, 0, 360);
 
@@ -51,32 +54,53 @@ function draw() {
   noStroke();
   // fill(5, 99, 35);
   fill(color);
-  ellipse(0, 0, 20, 20);
+  ellipse(0, 0, 25, 25);
 
-  //sun arc
-  stroke(255, 164, 36);
+  //hour arc
+  stroke(184, 2, 150);
   strokeWeight(2);
   noFill();
-  arc(0, 0, 400, 400, 360, 360);
+  arc(0, 0, 300, 300, 0, hoursArc);
+
+  //min arc
+  stroke(184, 2, 150);
+  strokeWeight(2);
+  noFill();
+  arc(0, 0, 200, 200, 0, minutesArc);
+
+  //sec arc
+  stroke(184, 2, 150);
+  strokeWeight(2);
+  noFill();
+  arc(0, 0, 100, 100, 0, secondsArc);
+
+  //sun arc
+
+  stroke(255, 140, 0);
+
+  strokeWeight(2);
+  noFill();
+  arc(0, 0, 850, 850, 360, 360);
 
   //moon arc
   noFill();
-  stroke(53, 48, 74);
+  stroke(197, 197, 227);
   strokeWeight(2);
-  arc(0, 0, 80, 80, 0, 360);
+  arc(0, 0, 600, 600, 0, 360);
 
   //month ellipse
   push();
   rotate(dayArc);
   fill(97, 84, 153);
-  ellipse(0, 40, 15, 15);
+  ellipse(0, 300, 25, 25);
 
   pop();
 
   //year ellipse
   push();
   rotate(monthsArc);
-  fill(208, 20, 70);
-  ellipse(0, 200, r, r);
+  stroke(245, 69, 80);
+  fill(255, 164, 36);
+  ellipse(0, 425, r, r);
   pop();
 }
